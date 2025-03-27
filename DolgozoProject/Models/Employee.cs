aa19920fedaf4685c5939decd7381b38a7a7d38f
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-03-27 10:25:53 +0100

    Add project files.

---
+++ DolgozoProject/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozoProject.Models
+{
+    public class Employee
+    {
+        private string _email;
+        private int _salary;
+
+        public Employee(string email, string name)
+        {
+            if(string.IsNullOrEmpty(email) || string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Email and name must be provided");
+            }
+            _email = email;
+            Name = name;
+        }
+
+        public string Name { get; set; }
+        public double Adokulcs => 0.27;
+        public double Ado => _salary*Adokulcs;
+        public int TimesPaid { get; private set; } = 0;
+
+        public void IncreaseSalary(int amount)
+        {
+            if(amount < 0)
+            {
+                throw new ArgumentException("Amount must be positive");
+            }
+            _salary += amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}\n{_email}\n{_salary}\n{Ado}\n{TimesPaid}";
+        }
+    }
+}
---
+++ DolgozoProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using DolgozoProject.Models;
+
+try
+{
+    Employee emptye = new Employee("", "John Doe");
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+Employee anna = new Employee("nagy.anna@munkahely.com","Nagy Anna");
+try
+{
+    anna.IncreaseSalary(-5000);
+}catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+anna.IncreaseSalary(25000);
+anna.IncreaseSalary(40000);
+Console.WriteLine(anna);
+Console.WriteLine($"{anna.Adokulcs}; { anna.Ado}");
+Console.WriteLine(anna.TimesPaid);
