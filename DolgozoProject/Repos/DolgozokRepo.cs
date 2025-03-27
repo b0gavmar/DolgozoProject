b1028775bb3036d3a8b2b4a87a61e92e685d823f
2020b Gavlik Martin <144203996+b0gavmar@users.noreply.github.com>
2025-03-27 10:32:20 +0100

    4. fix

---
+++ DolgozoProject/Repos/DolgozokRepo.cs
@@ -0,0 +1,30 @@
+﻿using DolgozoProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozoProject.Repos
+{
+    public class DolgozokRepo
+    {
+        private readonly DolgozokContext _context;
+
+        public DolgozokRepo(DolgozokContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> GetNumberOfEmployees()
+        {
+            return await _context.Dolgozok.CountAsync();
+        }
+
+        public async Task<List<Employee>> GetAll()
+        {
+            return await _context.Dolgozok.ToListAsync();
+        }
+    }
+}
